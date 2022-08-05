ce1432471d0280b97a186f6e5f9b67a5335db1a2
Ikbal Yaduar Taupik <Ikbal.ytaufik@gmail.com>
2022-08-05 09:23:31 +0700

    add File KeyWordVar

---
+++ src/KeyWordVar.java
@@ -0,0 +1,16 @@
+public class KeyWordVar {
+    public static void main(String[] args) {
+
+        // Wrong example while using Key Word var
+        // var name;
+        // name = "Ikbal Yaduar Taupik";
+
+        var name = "Ikbal";
+        var age = 23;
+        var address = "Indonesia";
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(address);
+    }
+}
