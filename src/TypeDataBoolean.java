9c35a143535507c8f7080eab68e5076024da4d7e
Ikbal Yaduar Taupik <Ikbal.ytaufik@gmail.com>
2022-08-05 08:57:00 +0700

    add Type data char and boolean

---
+++ src/TypeDataBoolean.java
@@ -0,0 +1,10 @@
+public class TypeDataBoolean {
+    public static void main(String[] args) {
+
+        boolean booleanTrue = true;
+        boolean booleanFalse = false;
+
+        System.out.println(booleanTrue);
+        System.out.println(booleanFalse);
+    }
+}
---
+++ src/TypeDataChar.java
@@ -0,0 +1,10 @@
+public class TypeDataChar {
+    public static void main(String[] args) {
+
+        char i = 'I';
+        char k = 'K';
+
+        System.out.println(i);
+        System.out.println(k);
+    }
+}
