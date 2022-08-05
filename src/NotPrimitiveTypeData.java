c3660d3910dda19a9c73ff87517dc90b929d40e6
Ikbal Yaduar Taupik <Ikbal.ytaufik@gmail.com>
2022-08-05 09:38:35 +0700

    add File NotPrimitiveTypeData

---
+++ src/NotPrimitiveTypeData.java
@@ -0,0 +1,11 @@
+public class NotPrimitiveTypeData {
+    public static void main(String[] args) {
+
+        Byte iniByte = 100;
+        Short iniShort = 1000;
+        Integer iniInteger;
+        iniInteger = null;
+
+        System.out.println(iniInteger);
+    }
+}
