8f0eb3d83767db57b4f9ba2a383a0e8588a71a92
Ikbal Yaduar Taupik <Ikbal.ytaufik@gmail.com>
2022-08-05 09:12:06 +0700

    add File Variable

---
+++ src/Variable.java
@@ -0,0 +1,15 @@
+public class Variable {
+    public static void main(String[] args) {
+
+        String name;
+        name = "Ikbal Yaduar Taupik";
+
+        int age = 23;
+
+        String address = "Indonesia";
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(address);
+    }
+}
