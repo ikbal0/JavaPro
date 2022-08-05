c949274a8e58938d0728ed192eb91182f8f81532
Ikbal Yaduar Taupik <Ikbal.ytaufik@gmail.com>
2022-08-05 15:23:05 +0700

    add method to MethodReturnValue

--- src/MethodReturnValue.java
+++ src/MethodReturnValue.java
@@ -5,10 +5,23 @@ public class MethodReturnValue {
         var c = sum(a, b);
 
         System.out.println(c);
+        System.out.println(check(100, "+", 100));
+        System.out.println(check(200, "-", 100));
     }
 
     static int sum(int value1, int value2){
         var total = value1 + value2;
         return total;
+    }
+
+    static  int check(int value1, String operation, int value2){
+        switch (operation){
+            case "+":
+                return value1 + value2;
+            case "-":
+                return value1 - value2;
+            default:
+                return 0;
+        }
     }
 }
