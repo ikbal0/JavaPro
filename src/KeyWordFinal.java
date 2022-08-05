5a9deed42a60e52a98d523e420549c2f04d9e089
Ikbal Yaduar Taupik <Ikbal.ytaufik@gmail.com>
2022-08-05 09:27:29 +0700

    add File KeyWordFinal

---
+++ src/KeyWordFinal.java
@@ -0,0 +1,8 @@
+public class KeyWordFinal {
+    public static void main(String[] args) {
+
+        final String name = "Ikbal"; // now variable name can't be changed because it's using final key word
+
+        System.out.println(name);
+    }
+}
