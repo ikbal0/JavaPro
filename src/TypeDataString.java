1fcd8ab2b68c6c6231a3c654cb2edd810b5d3714
Ikbal Yaduar Taupik <Ikbal.ytaufik@gmail.com>
2022-08-05 09:02:25 +0700

    add Type data String

---
+++ src/TypeDataString.java
@@ -0,0 +1,10 @@
+public class TypeDataString {
+    public static void main(String[] args) {
+        String firstName = "Ikbal";
+        String middleName = "Yaduar";
+        String lastName = "Taupik";
+        String fullName = firstName + " " + middleName + " " + lastName;
+
+        System.out.println(fullName);
+    }
+}
