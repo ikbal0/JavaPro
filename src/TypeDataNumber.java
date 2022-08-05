f3e2b2e57ab4ece2568c7a116011f198c387f7cd
Ikbal Yaduar Taupik <Ikbal.ytaufik@gmail.com>
2022-08-05 08:24:02 +0700

    rename from tipe to type and add uiDesigner.xml

--- src/TypeDataNumber.java
+++ src/TypeDataNumber.java
@@ -1,4 +1,4 @@
-public class TipeDataNumber {
+public class TypeDataNumber {
     public static void main(String[] args) {
 
         // total capacity byte type data is from -128 to 127, size 1 byte, default value 0
@@ -13,5 +13,11 @@ public class TipeDataNumber {
         // total capacity long type data is from -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807, size 8 byte, default value 0
         long iniLOng = 1000000000;
         long iniLong2 = 100000000L;
+
+        // total capacity float type data is from 3.4e-038 to 3.4e+038, size 4 byte, default value 0.0
+        float iniFloat = 10.12F;
+
+        // total capacity double type data is from 1.7e-308 to 1.7e+308, size 8 byte, default value 0.0
+        double iniDouble = 12.2434;
     }
 }
